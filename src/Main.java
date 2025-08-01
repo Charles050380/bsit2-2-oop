6d66ad99a4ca64e8fc246a60d36bb533eeb0002c
Charles David A. Navarez <cdnavarez29386@liceo.edu.ph>
2025-08-01 09:46:06 +0800

    activity lab 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int TotalNgaNumber = 0;
 
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int NumberNgaIscan = scanner.nextInt();
+            TotalNgaNumber += NumberNgaIscan;
+        }
+
+        System.out.println("\nTotal Number: " + TotalNgaNumber);
+
+        scanner.close();
+    }
+}
